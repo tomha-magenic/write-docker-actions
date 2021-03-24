f6ec6bac01b96d73d5b1ff451004fba0ca01571c
tomha-magenic <tomha@magenic.com>
2021-03-23 22:50:04 -0500

    main.go update

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello Docker Actions")
+
+	// Access Inputs as environment vars
+	firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
+	secondGreeting := os.Getenv("INPUT_SECONDGREETING")
+	thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
+
+	// Use those inputs in the action logic
+
+	fmt.Println("Hello " + firstGreeting)
+	fmt.Println("Hello " + secondGreeting)
+
+	if thirdGreeting != "" {
+		fmt.Println("Hello " + thirdGreeting)
+	}
 }
